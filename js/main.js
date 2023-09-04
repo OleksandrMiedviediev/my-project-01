62920ec8a0ef53a853cc5b7c9563e343fa116a07
OleksandrMiedviediev <119736072+OleksandrMiedviediev@users.noreply.github.com>
2023-09-04 17:24:59 +0200

    upp

--- js/main.js
+++ js/main.js
@@ -8,3 +8,13 @@
         }
     };
 }());
+
+//Burger hangler
+(function () {
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('header_nav');
+    const menuCloseItem = document.querySelector('.header_nav_close');
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('header_nav_active');
+    });
+}());
