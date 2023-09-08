5aa3b3d96888093504b8deaa0f62f2e26b371779
OleksandrMiedviediev <119736072+OleksandrMiedviediev@users.noreply.github.com>
2023-09-08 02:38:26 +0200

    upp

--- js/main.js
+++ js/main.js
@@ -12,9 +12,12 @@
 //Burger hangler
 (function () {
     const burgerItem = document.querySelector('.burger');
-    const menu = document.querySelector('header_nav');
+    const menu = document.querySelector('.header_nav');
     const menuCloseItem = document.querySelector('.header_nav_close');
     burgerItem.addEventListener('click', () => {
         menu.classList.add('header_nav_active');
     });
+    menuCloseItem.addEventListener('click', () => {
+        menu.classList.remove('header_nav_active');    
+    })
 }());
